5abbb93aaadce4660044f2de03983baf27c6c3ac
Cho-Yeung Lam <jackie_lam@foxmail.com>
2013-12-25 10:12:28 +0800

    Implement with combination of C and Obj-C

---
+++ Encryption/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Encryption
+//
+//  Created by Cho-Yeung Lam on 24/10/13.
+//  Copyright (c) 2013 Cho-Yeung Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
